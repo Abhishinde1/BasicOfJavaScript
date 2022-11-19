1c6ca2ffda2839053a55d43612830d1bf2bc53e1
AbhiShinde1 <abhishinde572@gmail.com>
2022-11-19 20:31:05 +0530

    [Added- Basic code of arrayoperations]

---
+++ arrayoperations.js
@@ -0,0 +1,81 @@
+var arrayoperations;
+
+let names = ["Abhi", "Yash", "yogesh", "Akshay"];
+ //it will find the length
+//console.log(names.length);   
+
+//it will find the index value
+// let index = names[1];
+// console.log(index);
+
+//It will the last index
+// let last = names[names.length - 1];
+// console.log(last);
+
+//loop over an array 
+//it will print all data of an array
+// names.forEach((data) => {
+//     console.log(data);
+// });
+
+//it will print all data with index of an array
+// names.forEach((data, index) => {
+//     console.log(data, index);
+// });
+
+//Add an item to the end of an array
+// names.push("Shyam");
+// console.log("added", names);
+
+//Remove an item from end of an array
+// let last = names.pop();
+// console.log("removed last name", names);
+
+
+//Remove an array from starting 
+// let first = names.shift();
+// console.log("Removed starting name" , names);
+
+//Add an element to the starting of an array
+// let first = names.unshift("Abhishek");
+// console.log("Added in starting " , names);
+
+
+//find the index of an item in an array
+// let posi = names.indexOf("Akshay");
+// console.log("index", posi);
+
+
+//Splice will do element remove,Add at a time.....
+// const fruits = ["Apple", "Mango", "Pineapple"];
+// fruits.splice(0,2,"Banana");
+// console.log(fruits);
+
+
+//slice
+// let text = "Good morning";
+// let afterslice = text.slice(4, 12);     
+// console.log(afterslice);
+
+//Split 
+// let text = "Good morning";
+// let afterslice = text.split(" ");
+// console.log(afterslice[1]);
+
+//Concat
+// const  district = ["pune", "mumbai"];
+// const  district1 = ["kolhapur"];
+// const  district2 = ["Nashik"];
+
+// const newArray = district.concat(district1,district2);
+// console.log(newArray);
+
+//join it will join the array element
+//2.and as per are choice we can give any seperator
+
+// const fruits = ["Apple", "Banana"];
+//let joins = fruits.join(" $ ");
+// let joins = fruits.toString();  //tostring  will just sperate by , only
+// console.log(joins)
+
+
