01922c5ae262fe72381492ff5b8647a89c97bd4a
AbhiShinde1 <abhishinde572@gmail.com>
2022-11-19 20:37:09 +0530

    Refactor code SySynchronizeandAsynchronize

--- SynchronizeandAsynchronize.js
+++ SynchronizeandAsynchronize.js
@@ -1,17 +1,25 @@
 // 1. Synchronize
 
-console.log("hello");
- console.log("Abhi");
- console.log("I am in CodeWave Technology");
+ console.log("hello");
+  console.log("Abhi");
+  console.log("I am in CodeWave Technology");
+
+
+
+const prompt = require("prompt-sync")();
+ let x = prompt("what is your name");
+ let y = prompt("what is your favourite color");
+ let z = prompt("what is your age");
+ console.log( "My name is" + x  + " favourite color is "+ y + " and my age is. " + z );
 
 
 //2.Asynchronize
 
-console.log("hello");
+// console.log("hello");
 
-setTimeout(function(){
-    console.log("Abhi")
-},3000)
-console.log("I am in CodeWave Technology");
+// setTimeout(function(){
+//     console.log("Abhi")
+// },3000)
+// console.log("I am in Bridgelabz");
 
 
